08f3488741658a4717a1fd4284e6bea49f494ded
Debaditya Malakar <debadityamalakar@outlook.com>
2024-12-10 00:10:33 +0530

    Part 4

--- src/bootloader/stage2/stdio.h
+++ src/bootloader/stage2/stdio.h
@@ -1,16 +1,6 @@
-#include "stdio.h"
-#include "x86.h"
+#pragma once
 
-void putc(char c)
-{
-    x86_Video_WriteCharTeletype(c, 0);
-}
-
-void puts(const char* str)
-{
-    while(*str)
-    {
-        putc(*str);
-        str++;
-    }
-}
+void putc(char c);
+void puts(const char* str);
+void puts_f(const char far* str);
+void _cdecl printf(const char* fmt, ...);
